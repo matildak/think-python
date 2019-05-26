1c2c1889434c9c808fc3413917b49f55cc56b20d
Matilda <mematildas@gmail.com>
2019-05-26 20:28:42 +0200

    Chaper 2

---
+++ variablesExpressionsStatements.py
@@ -0,0 +1,68 @@
+# VALUES AND TYPES:
+
+# to get what type: type('Hello, World!')
+#                   type(44)
+
+#str, int and float (3.4)
+
+print(1,000,000) # = 1 0 0
+
+# VARIABLES: a name that refers to a value
+
+#assignment statement:
+message = 'And now for something different'
+n = 17
+pi = 3.14159265
+
+messageType = type(message)
+nType = type(n)
+piType = type(pi)
+
+print(messageType)
+print(nType)
+print(piType)
+
+#keywords:
+#Python 2 has 31 keywords:
+#and del from not while as elif global or with assert else if pass yield
+#break except import print class exec in raise continue finally is return
+#def for lambda try
+#In Python 3, exec is no longer a keyword, but nonlocal is.
+
+#OPERATORS AND OPERANDS
+# +, -, *, / and ** perform addition, subtraction, multiplication, division and exponentiation
+
+minute = 59
+print (minute/60)  # = 0.9833333333333333
+print (minute//60) # = 0
+
+#EXPRESSIONS AND STATEMMENTS
+#An expression is a combination of values, variables, and operators.
+#A statement is a unit of code that the Python interpreter can execute
+#an --expression-- has a value; a --statement-- does not.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
