d03267d58637a9a4b07b177dcedb555ab8e9dfd4
Matilda Karlsson <mematildas@gmail.com>
2021-05-18 20:11:31 +0200

    Update dict.py
    Added set()

--- dict.py
+++ dict.py
@@ -50,4 +50,45 @@ pp(f)
 
 
 
+#Set
+p = {6,35,65,78}
+print(type(p))
+
+d = {} #blir ju en tom dict
+#så
+e = set()
+print(e)
+s = set([2,4,16,64])
+print(s)
+t = [1,3,3,3,5,7,8,8]
+tt = set(t) #tar bort dubletter
+print(t)
+print(tt)
+
+tt.add(999)
+print(tt) #{1, 3, 5, 7, 8, 999}
+
+tt.update([1,2,3,4])
+print(tt) #{1, 2, 3, 4, 5, 7, 8, 999} Oftast inte i ordning!!
+#kolla med "2 in tt" eller "2 not in tt"
+tt.remove(1) #måste finnas annars error
+tt.discard(88)# behöver inte finnas
+
+j = tt.copy() #shallow copy (not objects)
+
+m = set(tt)
+
+# Sets kan jämföras för att se subset, mm
+#typ ett set med blonda och ett med blåa ögon
+#blue_eyes.union(blond_hair)
+#blue_eyes.intersection(blond_hair)
+#blue_eyes.difference(blond_hair)
+#symetric_difference
+#issubset
+#issuperset
+#isdisjoint
+
+
+
+
 
